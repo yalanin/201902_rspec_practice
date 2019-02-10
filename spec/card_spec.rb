82daecc60e109f7e898948c1fdfca8fbb1675606
yalanin <yalanin@gmail.com>
2019-02-10 18:57:55 +0800

    refactor spec code

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -10,18 +10,28 @@ end
 
 
 RSpec.describe Card do
+  # two ways to refactor the repeat code
+  
+  # before do
+  # before(:example) do
+  # 上二行功能相同
+  #   @card = Card.new('Ace', 'Spades')
+  # end
+
+  def card
+    Card.new('Ace', 'Spades')
+  end
+
   # it example describtion do
   # specify example describtion
   # 上二行功能相同      
   # end
 
-  it '包含一個數字'do
-    card = Card.new('Ace', 'Spades')
+  it '包含一個數字'do  
     expect(card.rank).to eq('Ace')
   end
 
   it '包含一個類別' do
-    card = Card.new('Ace', 'Spades')
     expect(card.suit).to eq('Spades')
   end
 end
