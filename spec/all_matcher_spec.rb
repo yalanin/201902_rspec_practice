8a06306cca41b6893c79b7a3c20ba2268a318a31
yalanin <yalanin@gmail.com>
2019-02-24 15:24:20 +0800

    all matcher method in rspec

---
+++ spec/all_matcher_spec.rb
@@ -0,0 +1,19 @@
+RSpec.describe 'all matcher' do
+  it 'allows for aggredate checks' do
+    # [5, 7, 9].each do |val|
+    #   expect(val).to be_odd
+    # end
+
+    # all matcher method in rspec
+    expect([5, 7, 9]).to all(be_odd)
+    expect([2, 4, 6, 8]).to all(be_even)
+    expect([[], [], []]).to all(be_empty)
+    expect([0, 0]).to all(be_zero)
+    expect([5, 7, 9]).to all(be < 10)
+  end
+
+  context [5, 7, 9] do
+    it { is_expected.to all(be_odd) }
+    it { is_expected.to all(be < 10) }
+  end
+end
