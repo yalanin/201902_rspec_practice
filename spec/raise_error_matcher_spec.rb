f8358e6e788a8215cc8f57961f80248e9d16c5d6
yalanin <yalanin@gmail.com>
2019-02-28 20:11:32 +0800

    raise_error method in rspec

---
+++ spec/raise_error_matcher_spec.rb
@@ -0,0 +1,37 @@
+RSpec.describe 'raise_error matcher' do
+  def some_method
+    x
+  end
+
+  class CustomError < StandardError; end
+
+  # 太籠統不要這樣用
+  # it 'can check any error' do
+  #   expect { some_method }.to raise_error
+  # end
+
+  it 'can check for a specific error being raised' do
+    expect { some_method }.to raise_error(NameError)
+    expect { 10/0 }.to raise_error(ZeroDivisionError)
+  end
+
+  it 'can check for user-create error' do
+    expect { raise CustomError }.to raise_error(CustomError)
+  end
+
+  # 這樣寫不會過，因為 it method 不會創建 class
+  # it CustomError do
+  #   expect { raise described_class }.to raise_error(CustomError)
+  # end
+
+  # 這樣寫會過，因為會出現此錯誤
+  it CustomError do
+    expect { raise described_class }.to raise_error(TypeError)
+  end
+
+  describe CustomError do
+    it 'can check for user-create error' do
+      expect { raise described_class }.to raise_error(CustomError)
+    end   
+  end
+end
